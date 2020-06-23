ae03058a6aac9b66006b1ee3dee859886f9761d1
chenlei0330 <245630186@qq.com>
2020-06-23 21:50:51 +0800

    the attempt of branch

--- clone.c
+++ clone.c
@@ -5,5 +5,6 @@ printf("pleased to input your number:\n");
 scanf("%d",&a);
 if(a>11)
 printf("you are good!have good life~");
+printf("this is the first attempt of diffrient branchs");
 return 0;
 }
